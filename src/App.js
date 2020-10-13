7e7b50d3a6f5aa0565ef0029c51b3c9dc510956b
pamelagrhz <pamelagrhz@gmail.com>
2020-10-13 17:14:34 -0500

    Props (pass information between components)

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import HelloWorld, { GoodBye } from "./components/HelloWorld.js";
+import Greet from "./components/Greet";
+
 
 function App() {
+  const userInfo = {
+    name: "Pamela",
+    lastName: "Ruíz",
+    age: "23"
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <HelloWorld />
         <GoodBye />
+        <Greet user={userInfo} />
       </header>
     </div>
   );
---
+++ src/components/Greet.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Greet(prop) {
+  console.log(prop);
+
+  return (
+    <div >
+      <h2 >Hola {prop.user.name} {prop.user.lastName}, Edad: {prop.user.age}</h2>
+    </div>
+
+
+
+  );
+}
