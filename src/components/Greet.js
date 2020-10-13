2018df329317de69300a4bfa616bc79a7772ca33
pamelagrhz <pamelagrhz@gmail.com>
2020-10-13 18:27:35 -0500

    Destructuring assignment

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 export default function Greet(prop) {
+  const { user, saludo } = prop;
+  const { name, lastName, age } = user;
   console.log(prop);
 
   return (
     <div >
-      <h2 >Hola {prop.user.name} {prop.user.lastName}, Edad: {prop.user.age}</h2>
-      <button onClick={() => prop.saludo(prop.user.name)}>Saludar</button>
+      <h2 >Hola {name} {lastName}, Edad: {age}</h2>
+      <button onClick={() => saludo(name)}>Saludar</button>
     </div>
 
 
