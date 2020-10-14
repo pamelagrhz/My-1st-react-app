192bd2ad2a544b6034e59b12bc337920c78f6bc1
pamelagrhz <pamelagrhz@gmail.com>
2020-10-13 19:11:29 -0500

    React bootstrap

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import HelloWorld, { GoodBye } from "./components/HelloWorld.js";
 import Greet from "./components/Greet";
+import { Button } from 'react-bootstrap';
 
 
 function App() {
@@ -23,6 +24,8 @@ function App() {
         <GoodBye />
         <Greet user={userInfo} saludo={saludo} />
       </header>
+      <h1>React Bootstrap</h1>
+      <Button variant="info" size="lg" block>Algo</Button>
     </div>
   );
 }
